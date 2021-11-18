f13ae4e97ac8d5ed71ece6f89c778e89dc5d9fde
Urja Srivastava <71330176+urjasrivastava@users.noreply.github.com>
2021-11-18 14:03:15 +0530

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -2,6 +2,7 @@ from torch.utils.data import Dataset
 import pandas as pd
 import os
 import torch
+from facenet_pytorch import InceptionResnetV1
 
 
 class VideoDataset(Dataset):
@@ -11,6 +12,36 @@ class VideoDataset(Dataset):
         self.dataframe = pd.read_csv(csv)
         self.transform = transform
         self.root = root
+        self.model_conv = InceptionResnetV1(pretrained='vggface2').eval()
+        
+
+    def __len__(self):
+        return len(self.dataframe)
+
+    def __getitem__(self, index):
+
+        #label = torch.tensor(self.dataframe.iloc[index][2])
+        video = os.path.join(self.root, self.dataframe.iloc[index].ClipID.replace(".avi","").replace(".mp4",""))
+        frames = self.transform(video)
+        frames = frames.unsqueeze(0);
+        z = torch.zeros([4, 512]).cuda()
+        for t in range(0,4):
+            x = self.model_conv(frames[:,t, :, :, :]) #batchsize*C*H*W
+            #x = self.model_linear(x) batchsize*512
+            z[t, :] = x #4*512
+
+        return z
+
+class EmbeddingDataset(Dataset):
+    
+    def __init__(self, csv, train):
+
+        self.dataframe = pd.read_csv(csv)
+        self.path=''
+        if train:
+            self.path = "data_drive_path"
+        else :
+            self.path = "test_drive_path"     
         
 
     def __len__(self):
@@ -19,7 +50,7 @@ class VideoDataset(Dataset):
     def __getitem__(self, index):
 
         label = torch.tensor(self.dataframe.iloc[index][2])
-        video = os.path.join(self.root, self.dataframe.iloc[index].ClipID.replace(".avi","").replace(".mp4",""))
-        frames = self.transform(video)
+        path = "{}{}".format(self.path,index)
+        z = torch.load(path)      
 
-        return frames, label
+        return z,label
