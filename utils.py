f66eb57c2a1c32254cd548bd2588af120fea661e
Urja Srivastava <71330176+urjasrivastava@users.noreply.github.com>
2021-11-18 14:07:12 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,31 +1,16 @@
-import torchvision
 from torch.utils.data import DataLoader
-import torch
 import datasets
-import transforms
 
 
-def generate_dataloaders(batch_size, csv, root,name):
-    dataset = datasets.VideoDataset(csv,
-                                    root,
-                                    transform=torchvision.transforms.Compose([transforms.VideoFolderPathToTensor()]))
-    torch.save(dataset,name)
+def generate_dataloaders(batch_size, csv, train):
+    dataset = datasets.EmbeddingDataset(csv,
+                                    train)
     return DataLoader(dataset,
                       batch_size=batch_size,
                       shuffle=True,drop_last=True)
 
-def generate_dataloader(batch_size,name):
-    dataset_loaded=torch.load(name)
-    return DataLoader(dataset_loaded,
-                      batch_size=batch_size,
-                      shuffle=True,drop_last=True)
 
-def get_dataloader(batch_size, csv_train, root_train, csv_test, root_test):
+def get_dataloaders(batch_size, csv_train, csv_test):
     return {
-        'train': generate_dataloader(batch_size,"train.pt"),
-        'test': generate_dataloader(batch_size,"test.pt")}
-
-def get_dataloaders(batch_size, csv_train, root_train, csv_test, root_test):
-    return {
-        'train': generate_dataloaders(batch_size, csv_train, root_train,"train.pt"),
-        'test': generate_dataloaders(batch_size, csv_test, root_test,"test.pt")}
+        'train': generate_dataloaders(batch_size, csv_train,True),
+        'test': generate_dataloaders(batch_size, csv_test,False)}
